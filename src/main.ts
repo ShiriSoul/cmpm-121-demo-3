924c01cd919507ab5aacb234cbdc5c54c8a6e26e
Tony Pau <topau@ucsc.edu>
2024-11-04 23:09:33 -0800

    L16 complete. Added button with alert.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-// todo
+import "./style.css";
+
+// creates the starting button element
+const starterButton = document.createElement("button");
+starterButton.textContent = "Click Me";
+starterButton.id = "starterButton";
+document.body.appendChild(starterButton);
+
+// starting button event listener to display message
+starterButton.addEventListener("click", () => {
+  alert("You clicked the button!");
+});
